99ae684309196346cf67565fdceb5eb1c97852ea
simpleito <simpleito@qq.com>
2019-03-16 12:42:53 +0800

    change decorator's logic

--- src/structural/decorator/Client.java
+++ src/structural/decorator/Client.java
@@ -1,12 +1,15 @@
 package structural.decorator;
 
+/**
+ * 透明装饰模式
+ */
 public class Client {
     public static void main(String[] args){
         Phone phone = new SimplePhone();
         phone.noticeCall();
         System.out.println("------------------------");
 
-        phone = new ComplexPhone(new JarPhone(phone));  // 或者添加一个构造方法再利用instance of，对客户来说也可以从SimplePhone直接装饰到ComplexPhone
+        phone = new JarPhone(phone);
         phone.noticeCall();
     }
 }
